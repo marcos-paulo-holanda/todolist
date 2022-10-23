e0bfdde50fa0ce95d094536ec235f6a8f30a6e5d
marcos <marcos.holanda@hotmail.com.br>
2022-10-23 19:40:45 -0300

    changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://admin-marcos:gogo123456@cluster0.eydy1v7.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String
